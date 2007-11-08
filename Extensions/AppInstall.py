0f8133e937825489bb57b11113f85ef87afdd2a6
Maurits van Rees <maurits@vanrees.org>
2007-11-08 16:43:13 +0000

    Do not add indexes in the catalog.xml but in AppInstall.py as we would then need to add some code there anyway for reindexing after a reinstall.
    svn path=/feedfeeder/trunk/; revision=53473

--- Extensions/AppInstall.py
+++ Extensions/AppInstall.py
@@ -2,12 +2,17 @@ from StringIO import StringIO
 from Products.CMFCore.utils import getToolByName
 
 
-def install(portal):
+def install(site):
+    out = StringIO()
+    applyGenericSetupProfile(site, out)
+    add_indexes(site, out)
+
+
+def applyGenericSetupProfile(site, out):
     """Just apply our own extension profile.
     """
-    out = StringIO()
 
-    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool = getToolByName(site, 'portal_setup')
     setup_tool.setImportContext('profile-feedfeeder:default')
     print >> out, "Applying the generic setup profile for feedfeeder..."
     setup_tool.runAllImportSteps(purge_old=False)
@@ -18,3 +23,23 @@ def install(portal):
         setup_tool.setImportContext('profile-Products.CMFPlone:plone')
     print >> out, "Applied the generic setup profile for feedfeeder"
 
+
+def add_indexes(site, out):
+    """Add indexes.
+
+    I have *had* it with catalog.xml for indexes.  If we have to add
+    some code here to reindex our indexes after catalog.xml has been
+    imported, we might as well add some code instead to only add them
+    when they are not there yet.
+
+    """
+    catalog = getToolByName(site, 'portal_catalog')
+    indexes = catalog.indexes()
+
+    idx = "getFeedItemUpdated"
+    if idx not in indexes:
+        # Setting index_naive_time_as_local to True does not seem
+        # possible with the 'extra' argument to addIndex, but True is
+        # the default, so we are happy.
+        catalog.addIndex(idx, 'DateIndex')
+        print >> out, 'Added DateIndex for %s.' % idx
