7014a97bfa3f60a61493c3d28c37034ccb348618
Maurits van Rees <maurits@vanrees.org>
2008-10-04 01:48:24 +0000

    Removed unused STYLESHEETS and JAVASCRIPTS.
    svn path=/feedfeeder/trunk/; revision=72991

--- Products/feedfeeder/config.py
+++ Products/feedfeeder/config.py
@@ -2,23 +2,15 @@
 
 # Product configuration.
 #
-# The contents of this module will be imported into __init__.py, the
+# The contents of this module is imported into __init__.py, the
 # workflow configuration and every content type module.
-#
-# If you wish to perform custom configuration, you may put a file
-# AppConfig.py in your product's root directory. This will be included
-# in this file if found.
 
 from Products.CMFCore.permissions import setDefaultRoles
 
 
 PROJECTNAME = "feedfeeder"
 
-# Permissions
 DEFAULT_ADD_CONTENT_PERMISSION = "%s: Add" % PROJECTNAME
 setDefaultRoles(DEFAULT_ADD_CONTENT_PERMISSION, ('Manager', 'Owner'))
 
 product_globals = globals()
-
-STYLESHEETS = []
-JAVASCRIPTS = []
