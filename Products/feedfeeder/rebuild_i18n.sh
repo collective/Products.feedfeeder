80ab746494e70634404250f265c07d9b30e4b0f5
Maurits van Rees <maurits@vanrees.org>
2009-05-09 21:58:21 +0000

    Activate code in rebuild_i18n.sh to sync the plone domain translations.
    svn path=/feedfeeder/trunk/; revision=85758

--- Products/feedfeeder/rebuild_i18n.sh
+++ Products/feedfeeder/rebuild_i18n.sh
@@ -8,9 +8,7 @@ I18NDOMAIN=$PRODUCTNAME
 i18ndude rebuild-pot --pot i18n/${PRODUCTNAME}.pot --create ${I18NDOMAIN} --merge i18n/generated.pot skins/${PRODUCTNAME}
 
 # Synchronise the resulting .pot with the .po files
-i18ndude sync --pot i18n/${PRODUCTNAME}.pot i18n/${PRODUCTNAME}-??.po
-# Add this for updating any future language variant files, like pt-br
-# files: i18n/${PRODUCTNAME}-??-??.po
+i18ndude sync --pot i18n/${PRODUCTNAME}.pot i18n/${PRODUCTNAME}-*.po
 
-# Synchronise the plone*.po files with the hand-made plone-PRODUCTNAME.pot
-#i18ndude sync --pot i18n/plone-${PRODUCTNAME}.pot i18n/plone*.po
+# Synchronise the plone-*.po files with the hand-made plone.pot
+i18ndude sync --pot i18n/plone.pot i18n/plone-*.po
