379cf79b2fa4954487d21dfa2b475b223816cd67
Davide Moro <davide.moro@gmail.com>
2010-12-06 09:20:47 +0000

    redirect of item only if you don't have edit permissions.
    svn path=/feedfeeder/branches/redomino/; revision=228284

--- Products/feedfeeder/browser/feeditem.py
+++ Products/feedfeeder/browser/feeditem.py
@@ -1,5 +1,6 @@
 from zope.interface import Interface
 from zope.interface import implements 
+from zope.component import getMultiAdapter
 
 from Products.Five import BrowserView
 
@@ -10,11 +11,16 @@ class IFeedItemView(Interface):
 
     def redirect_url():
         """ Returns empty string or the url to be redirected, depending on
-            the configuration of the feed folder
+            the configuration of the feed folder.
+            Returns an empty string if redirect is not enabled or if you don't have modify 
+            permissions
         """
 
     def parent():
         """ Returns the feed item parent """
+
+    def checkEditPermission():
+        """ Returns if you have the edit permission"""
 
 
 class FeedItemView(BrowserView):
@@ -24,6 +30,8 @@ class FeedItemView(BrowserView):
       >>> verifyClass(IFeedItemView, FeedItemView)
       True
 
+      Fake objects
+      ------------
       >>> fake_requets = {}
       >>> class FakeFolder(object):
       ...     def __init__(self, redirect):
@@ -37,21 +45,41 @@ class FeedItemView(BrowserView):
       ...         return {'link':'http://somewhere'}
       ...     def getFeedFolder(self):
       ...         return self.folder 
+      >>> class FakeFeedItemView(FeedItemView):
+      ...     def checkEditPermission(self):
+      ...         return True
 
       Test redirects
       --------------
       Item with a folder with redirect enabled
       >>> item1 = FakeItem(True)
-      >>> view1 = FeedItemView(item1, fake_requets)
+      >>> view1 = FakeFeedItemView(item1, fake_requets)
       >>> view1.redirect_url()
       'http://somewhere'
 
       Item with a folder with redirect not enabled
       >>> item2 = FakeItem(False)
-      >>> view2 = FeedItemView(item2, fake_requets)
+      >>> view2 = FakeFeedItemView(item2, fake_requets)
       >>> bool(view2.redirect_url())
       False
 
+      Test redirects with no edit permissions
+      ---------------------------------------
+      >>> class FakeFeedItemView(FeedItemView):
+      ...     def checkEditPermission(self):
+      ...         return False
+
+      Item with a folder with redirect enabled
+      >>> item1 = FakeItem(True)
+      >>> view1 = FakeFeedItemView(item1, fake_requets)
+      >>> bool(view1.redirect_url())
+      False
+
+      Item with a folder with redirect not enabled
+      >>> item2 = FakeItem(False)
+      >>> view2 = FakeFeedItemView(item2, fake_requets)
+      >>> bool(view2.redirect_url())
+      False
     """
     implements(IFeedItemView)
 
@@ -61,10 +89,15 @@ class FeedItemView(BrowserView):
     def redirect_url(self):
         object_info = self.context.getObjectInfo()
         parent = self.parent()
-        if parent.getRedirect():
+        if parent.getRedirect() and self.checkEditPermission():
             return object_info.get('link')
         else:
             return ''
 
     def parent(self):
         return self.context.getFeedFolder()
+
+    def checkEditPermission(self):
+        """ Returns if you have the edit permission"""
+        membership = getMultiAdapter((self.context, self.request), name=u'plone_tools').membership()
+        return membership.checkPermission('Modify portal content', self.context)
