84f8ece25a858e62823d948da5141a72e3139ced
Maurits van Rees <maurits@vanrees.org>
2012-10-14 02:15:32 +0200

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup, find_packages
 import os
 
-version = open(os.path.join("Products", "feedfeeder", "version.txt")).read()
+version = open(os.path.join("Products", "feedfeeder", "version.txt")).read().strip()
 version = version.strip()
-readme = open(os.path.join("Products", "feedfeeder", "README.txt")).read()
-history = open(os.path.join("Products", "feedfeeder", "HISTORY.txt")).read()
+readme = open(os.path.join("Products", "feedfeeder", "README.txt")).read().strip()
+history = open(os.path.join("Products", "feedfeeder", "HISTORY.txt")).read().strip()
 
 setup(name='Products.feedfeeder',
       version=version,
       description="Turn external feed entries into content items",
-      long_description= readme + "\n" + history,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description= readme + "\n\n" + history,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 3.2",
-        "Framework :: Plone :: 3.3",
-        "Framework :: Plone :: 4.0",
-        "Framework :: Plone :: 4.1",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.2",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
       keywords='',
       author='Zest Software',
       author_email='m.van.rees@zestsoftware.nl',
