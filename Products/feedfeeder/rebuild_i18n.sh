30bd9b5e564f69ca3663e420b311576358da3bc7
Maurits van Rees <maurits@vanrees.org>
2012-12-14 01:07:27 +0100

    translate the plone domain in portal_atct

--- Products/feedfeeder/rebuild_i18n.sh
+++ Products/feedfeeder/rebuild_i18n.sh
@@ -8,5 +8,8 @@ i18ndude rebuild-pot --pot i18n/${PRODUCTNAME}.pot --create ${I18NDOMAIN} skins/
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot i18n/${PRODUCTNAME}.pot i18n/${PRODUCTNAME}-*.po
 
-# Synchronise the plone-*.po files with the hand-made plone.pot
+# Search for plone domain entries specifically in the profiles.
+i18ndude rebuild-pot --pot i18n/plone.pot --create plone profiles/
+
+# Synchronise the plone-*.po files with the plone.pot
 i18ndude sync --pot i18n/plone.pot i18n/plone-*.po
