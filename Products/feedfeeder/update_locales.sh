b4a606f6cb61c26309bc02c1f43d0643d8ddb691
Maurits van Rees <maurits@vanrees.org>
2012-12-21 23:46:55 +0100

    update_locales.sh

--- Products/feedfeeder/update_locales.sh
+++ Products/feedfeeder/update_locales.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
-PRODUCTNAME=feedfeeder
-I18NDOMAIN=$PRODUCTNAME
+#! /bin/sh
+i18ndude rebuild-pot --pot locales/feedfeeder.pot --create feedfeeder .
+# --merge locales/manual.pot
 
-# Synchronise the .pot with the templates.
-i18ndude rebuild-pot --pot i18n/${PRODUCTNAME}.pot --create ${I18NDOMAIN} skins/${PRODUCTNAME} ./
-
-# Synchronise the resulting .pot with the .po files
-i18ndude sync --pot i18n/${PRODUCTNAME}.pot i18n/${PRODUCTNAME}-*.po
+for po in locales/*/LC_MESSAGES/feedfeeder.po; do
+    i18ndude sync --pot locales/feedfeeder.pot $po
+done
 
 # Search for plone domain entries specifically in the profiles.
-i18ndude rebuild-pot --pot i18n/plone.pot --create plone profiles/
+i18ndude rebuild-pot --pot locales/plone.pot --create plone profiles/
 
 # Synchronise the plone-*.po files with the plone.pot
-i18ndude sync --pot i18n/plone.pot i18n/plone-*.po
+for po in locales/*/LC_MESSAGES/plone.po; do
+    i18ndude sync --pot locales/plone.pot $po
+done
