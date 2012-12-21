4a3e599123440251a53513b804d0a17b5d74f66b
Maurits van Rees <maurits@vanrees.org>
2012-12-22 00:00:40 +0100

    Check po files

--- Products/feedfeeder/update_locales.sh
+++ Products/feedfeeder/update_locales.sh
@@ -13,3 +13,9 @@ i18ndude rebuild-pot --pot locales/plone.pot --create plone profiles/
 for po in locales/*/LC_MESSAGES/plone.po; do
     i18ndude sync --pot locales/plone.pot $po
 done
+
+# Run checks
+for po in $(find . -name '*po'); do
+    msgfmt -c $po;
+done
+rm messages.mo
