870a5688e41fa8a9bba9db276172ac64e5699c34
Maurits van Rees <maurits@vanrees.org>
2014-12-26 16:28:06 +0100

    Back to development: 2.8

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@ History of feedfeeder
 =====================
 
 
+2.8 (unreleased)
+----------------
+
+- Nothing changed yet.
+
+
 2.7 (2014-12-26)
 ----------------
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open("README.txt").read().strip()
 history = open("CHANGES.rst").read().strip()
 
 setup(name='Products.feedfeeder',
-      version='2.7',
+      version='2.8.dev0',
       description="Turn external feed entries into content items",
       long_description= readme + "\n\n" + history,
       # Get more strings from
